e598a76b1c7868f6d537dfd6f5fc84b5007a63a8
tiagosutter <tiagosutter@gmail.com>
2022-01-14 22:06:22 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -2,16 +2,25 @@ import Pagination from "../../components/Pagination";
 import MovieCard from "../../components/MovieCard";
 import axios from "axios";
 import {BASE_URL} from "../../utils/requests";
+import {useEffect, useState} from "react";
+import {Movie, MoviePage} from "../../types/Movie";
 
 function Listing() {
 
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
-        .then(response => {
-            console.log(response.data)
-        });
+    const [pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() =>  {
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
+            .then(response => {
+                const data = response.data as MoviePage;
+                console.log(data);
+                setPageNumber(data.number);
+            });
+    }, [pageNumber])
 
     return (
         <>
+            <p>{pageNumber}</p>
             <Pagination />
 
             <div className="container">
