2b3e59e3d33ba82a79ee7aa86b13f48c5ac5e0c2
tiagosutter <tiagosutter@gmail.com>
2022-01-23 19:22:58 -0300

    Adds menu with Github icon

--- mobile/app/src/main/java/com/tiagoexemplo/dsmovie/MainActivity.kt
+++ mobile/app/src/main/java/com/tiagoexemplo/dsmovie/MainActivity.kt
@@ -2,10 +2,18 @@ package com.tiagoexemplo.dsmovie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+
+        menuInflater.inflate(R.menu.main, menu)
+
+        return super.onCreateOptionsMenu(menu)
     }
 }
