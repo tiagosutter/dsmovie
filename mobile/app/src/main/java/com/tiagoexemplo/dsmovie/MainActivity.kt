bd0b76b61583756e6245bdfcfe6eae6eabff68ec
tiagosutter <tiagosutter@gmail.com>
2022-01-24 18:50:34 -0300

    Adds MoviesAdapter and some fake data

--- mobile/app/src/main/java/com/tiagoexemplo/dsmovie/MainActivity.kt
+++ mobile/app/src/main/java/com/tiagoexemplo/dsmovie/MainActivity.kt
@@ -3,11 +3,28 @@ package com.tiagoexemplo.dsmovie
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MoviesAdapter.Interaction {
+
+    val moviesRecyclerView: RecyclerView by lazy { findViewById(R.id.moviesRecyclerView) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val moviesAdapter = MoviesAdapter(this)
+        moviesRecyclerView.adapter = moviesAdapter
+        moviesAdapter.submitList(listOf(
+            Movie(1, "The Witcher", 1.0, 1, "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg"),
+            Movie(1, "Venom: Tempo de Carnificina", 1.3, 3, "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/vIgyYkXkg6NC2whRbYjBD7eb3Er.jpg"),
+            Movie(1, "O Espetacular Homem-Aranha 2: A Ameaça de Electro", 2.0, 2, "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/u7SeO6Y42P7VCTWLhpnL96cyOqd.jpg"),
+            Movie(1, "Matrix Resurrections", 2.3, 45, "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/hv7o3VgfsairBoQFAawgaQ4cR1m.jpg"),
+            Movie(1, "Shang-Chi e a Lenda dos Dez Anéis", 3.0, 100, "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/cinER0ESG0eJ49kXlExM0MEWGxW.jpg"),
+            Movie(1, "Django Livre", 3.3, 2469, "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/2oZklIzUbvZXXzIFzv7Hi68d6xf.jpg"),
+            Movie(1, "Titanic", 4.0, 1, "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/yDI6D5ZQh67YU4r2ms8qcSbAviZ.jpg"),
+            Movie(1, "O Lobo de Wall Street", 4.3, 30, "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/cWUOv3H7YFwvKeaQhoAQTLLpo9Z.jpg"),
+            Movie(1, "Aves de Rapina: Arlequina e sua Emancipação Fantabulosa", 5.0, 67, "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jiqD14fg7UTZOT6qgvzTmfRYpWI.jpg"),
+        ))
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -15,5 +32,9 @@ class MainActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.main, menu)
 
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onRateClicked(item: Movie) {
+        TODO("Not yet implemented")
     }
 }
---
+++ mobile/app/src/main/java/com/tiagoexemplo/dsmovie/MoviesAdapter.kt
@@ -0,0 +1,114 @@
+package com.tiagoexemplo.dsmovie
+
+import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+
+
+class MoviesAdapter(private val interaction: Interaction) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+
+    private val diffCallback = object : DiffUtil.ItemCallback<Movie>() {
+
+        override fun areItemsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+    private val differ = AsyncListDiffer(this, diffCallback)
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
+        return MoviesViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_movie,
+                parent,
+                false
+            ),
+            interaction
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is MoviesViewHolder -> {
+                holder.bind(differ.currentList.get(position))
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    fun submitList(list: List<Movie>) {
+        differ.submitList(list)
+    }
+
+    class MoviesViewHolder(itemView: View, private val interaction: Interaction) :
+        RecyclerView.ViewHolder(itemView) {
+
+        private val movieItemImageView: ImageView = itemView.findViewById(R.id.movieItemImageView)
+        private val movieItemTitle: TextView = itemView.findViewById(R.id.movieItemTitle)
+        private val movieItemScore: TextView = itemView.findViewById(R.id.movieItemScore)
+        private val movieItemRatingCount: TextView = itemView.findViewById(R.id.movieItemRatingCount)
+        private val movieItemRateButton: Button = itemView.findViewById(R.id.movieItemRateButton)
+
+        private val movieItemStar1: ImageView = itemView.findViewById(R.id.movieItemStar1)
+        private val movieItemStar2: ImageView = itemView.findViewById(R.id.movieItemStar2)
+        private val movieItemStar3: ImageView = itemView.findViewById(R.id.movieItemStar3)
+        private val movieItemStar4: ImageView = itemView.findViewById(R.id.movieItemStar4)
+        private val movieItemStar5: ImageView = itemView.findViewById(R.id.movieItemStar5)
+
+        private val startViews = listOf(movieItemStar1, movieItemStar2, movieItemStar3,
+            movieItemStar4, movieItemStar5)
+
+        // TODO: Deal with I18n
+        @SuppressLint("SetTextI18n")
+        fun bind(movie: Movie) {
+            movieItemTitle.text = movie.title
+            movieItemScore.text = "%.2f".format(movie.score)
+            movieItemRatingCount.text = "${movie.count} Avaliações"
+
+            val scoreIntPart = movie.score.toInt()
+            val scoreDecimalPart = movie.score - scoreIntPart
+
+            for (i in 0 until scoreIntPart) {
+                startViews[i].setImageResource(R.drawable.star_full)
+            }
+
+            if (scoreDecimalPart > 0 && scoreIntPart != 5) {
+                startViews[scoreIntPart].setImageResource(R.drawable.star_half)
+            }
+
+            Glide.with(itemView)
+                .load(movie.image)
+                .into(movieItemImageView)
+
+            movieItemRateButton.setOnClickListener { interaction.onRateClicked(movie) }
+        }
+    }
+
+    interface Interaction {
+        fun onRateClicked(item: Movie)
+    }
+}
