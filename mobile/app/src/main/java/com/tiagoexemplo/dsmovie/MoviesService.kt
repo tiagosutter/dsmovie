f6267d33d0073d72fd954e870c55620faece398c
tiagosutter <tiagosutter@gmail.com>
2022-01-26 12:51:52 -0300

    Implements saving the rating for a movie

--- mobile/app/src/main/java/com/tiagoexemplo/dsmovie/MoviesService.kt
+++ mobile/app/src/main/java/com/tiagoexemplo/dsmovie/MoviesService.kt
@@ -1,10 +1,15 @@
 package com.tiagoexemplo.dsmovie
 
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.PUT
 
 
 interface MoviesService {
     @GET("movies?size=12&page=0&sort=id")
     fun getMovies(): Call<MoviesResponse>
+
+    @PUT("scores")
+    fun saveRating(@Body score: Score): Call<Movie>
 }
--- mobile/app/src/main/java/com/tiagoexemplo/dsmovie/RateMovieActivity.kt
+++ mobile/app/src/main/java/com/tiagoexemplo/dsmovie/RateMovieActivity.kt
@@ -1,17 +1,32 @@
 package com.tiagoexemplo.dsmovie
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
+import android.util.Log
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 class RateMovieActivity : AppCompatActivity() {
+
+    private val TAG = "RateMovieActivity"
+
+    private lateinit var moviesService: MoviesService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rate_movie)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://dsmovie-tiago.herokuapp.com/")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+
+        moviesService = retrofit.create(MoviesService::class.java)
 
         val movie = intent.getSerializableExtra("movie") as Movie
 
@@ -26,5 +41,33 @@ class RateMovieActivity : AppCompatActivity() {
         Glide.with(this).load(movie.image).into(rateMovieImageView)
 
         rateMovieCancelButton.setOnClickListener { onBackPressed() }
+
+        rateMovieSaveButton.setOnClickListener {
+            val email = rateMovieEmailEditText.text.toString()
+            val rating = rateMovieRatingEditText.text.toString().toDouble()
+            val score = Score(movie.id, email, rating)
+            val saveRatingApiCall = moviesService.saveRating(score)
+
+            saveRatingApiCall.enqueue(object : Callback<Movie> {
+                override fun onResponse(call: Call<Movie>, response: Response<Movie>) {
+                    finish()
+                    showSuccessToast()
+                }
+
+                override fun onFailure(call: Call<Movie>, t: Throwable) {
+                    Log.e(TAG, "moviesService.getMovies", t)
+                    showErrorToast()
+                }
+            })
+        }
     }
+
+    private fun showSuccessToast() {
+        Toast.makeText(this, "Rating saved", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showErrorToast() {
+        Toast.makeText(this, "An error has occurred", Toast.LENGTH_SHORT).show()
+    }
+
 }
